af0b60edaca7ba7fa54f12302bee0d7c76bd41c9
Seu nome <halisterfernando@hotmail.com>
2022-11-17 19:20:02 -0300

    feat: :art: refatorada logica para hashear o password

--- app/backend/src/database/controllers/SigninController.ts
+++ app/backend/src/database/controllers/SigninController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { ISigninService } from '../services/SigninService';
-const bcrypt = require('bcrypt')
+import bcrypt from '../../helpers/bcrypt';
 
 
 export default class SigninController {
@@ -10,8 +10,7 @@ export default class SigninController {
     
     signin = async ( req: Request, res: Response) => {
         const { username, password } = req.body  
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const hashedPassword = await bcrypt.encryptPassword(password)
         const newUser = await this.userService.signin(username, hashedPassword);
         return res.status(StatusCodes.OK).json(newUser)
     }   
