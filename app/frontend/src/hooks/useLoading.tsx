e6051f86fded71a5240472430ff5a5780ba061d7
Seu nome <halisterfernando@hotmail.com>
2022-11-20 02:56:28 -0300

    refactor: :recycle: código refatorado

--- app/frontend/src/hooks/useLoading.tsx
+++ app/frontend/src/hooks/useLoading.tsx
@@ -4,19 +4,18 @@ import React, {useState, useEffect} from 'react'
 const useLoading = () => {
   const [loading, setLoading] = useState(false);
   const loginPath = window.location.href.includes('login')
-  const signinPath = window.location.href.includes('signin');
-  const userPath = window.location.href.includes('user')  
+  const signinPath = window.location.href.includes('signin');   
   const threeSeconds = 3000
 
   useEffect(() => {
-    if(loginPath || signinPath || userPath) {
+    if(loginPath || signinPath ) {
         setLoading(true)
     }
 
     setTimeout(() => {
       setLoading(false)
     }, threeSeconds)
-    }, [loginPath, signinPath, userPath])
+    }, [loginPath, signinPath])
 
   return {loading}
 }
