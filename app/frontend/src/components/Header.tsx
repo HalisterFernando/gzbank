703e6909476e4bd7401a29a6a9a6318557c59de3
Seu nome <halisterfernando@hotmail.com>
2022-11-27 10:31:12 -0300

    feat: :sparkles: incluído header e footer na página de login

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -2,11 +2,12 @@ import React, {useContext, useEffect} from 'react'
 import Links from './Links'
 import { userContext, UserContextType } from '../context/UserContext'
 import { getItem } from '../helpers/localStorage'
+import useCheckPath from '../hooks/useCheckPath'
 
 const Header = () => {
   
   const {user, saveUserData} = useContext(userContext) as UserContextType
-  
+  const {path: { login, signin }} = useCheckPath()
   useEffect(() => {
     const localStorageUser = getItem('user')
     if (localStorageUser) {
@@ -17,19 +18,30 @@ const Header = () => {
 
   return (
     <header className="
-    bg-black
-    w-full
-    h-28
-    flex
-    flex-col
-    items-center
-    justify-evenly
-    gap-2
+      bg-black
+      w-full
+      h-28
+      flex
+      flex-col
+      items-center
+      justify-evenly
+      gap-2
     ">
-        <span className='text-white font-semibold mt-1'>Olá {user.username}</span>
-       <div className='flex gap-4'>
-        <Links />
-       </div>
+      {
+        login || signin ? (
+          <span>
+            <h1 className='text-white text-xl font-bold'>Seja bem-vindo ao NG_APP</h1>
+          </span>
+        ) : (
+          <>
+            <span className='text-white font-semibold mt-1'>Olá {user.username}</span>
+            <div className='flex gap-4'>
+              <Links />
+            </div>
+          </>
+        )
+      }
+        
     </header>
   )
 }
--- app/frontend/src/pages/FormPage.tsx
+++ app/frontend/src/pages/FormPage.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Form from '../components/Form'
 import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import PageFooter from '../components/PageFooter'
 
 const FormPage = () => {
   return (
+    <div className='h-screen flex flex-col'>   
+    <Header />
     <Form />
+    <PageFooter />
+    </div>
   )
 }
 
