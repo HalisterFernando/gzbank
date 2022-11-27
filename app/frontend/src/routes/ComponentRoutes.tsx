86918f774f113ba690b06ee409bf91a98ec7dc4c
Seu nome <halisterfernando@hotmail.com>
2022-11-27 09:15:29 -0300

    feat: :zap: redireciona usuario da rota / para /login

--- app/frontend/src/routes/ComponentRoutes.tsx
+++ app/frontend/src/routes/ComponentRoutes.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import FormPage from '../pages/FormPage'
-import Welcome from '../pages/Welcome'
 import BalancePage from '../pages/BalancePage'
 import TransactionPage from '../pages/TransactionPage'
 import TransferPage from '../pages/TransferPage'
+import { Navigate } from 'react-router-dom'
 
 const ComponentRoutes = () => {   
   return (
     
       <Routes>
-        <Route path='/' element={ <Welcome/> } />
+        <Route path='*' element={ <Navigate to='/login' replace /> } />
         <Route path='/signin' element={ <FormPage /> } />
         <Route path='/login'  element={ <FormPage /> } />
         <Route path='/balance' element={ <BalancePage />} />
