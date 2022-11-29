8c4d61e751ed35cd6e945b9c3be1b83f4b21bd9c
Seu nome <halisterfernando@hotmail.com>
2022-11-29 06:21:50 -0300

    refactor: :fire: removido variáveis não utilizadas

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -5,9 +5,9 @@ const config: Options = {
   username: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || 'password',
   database: process.env.DB_NAME || 'ng_cash',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
+  host: process.env.DB_HOST || 'db',
+  port: Number(process.env.DB_PORT) || 3302,
+  dialect: 'postgres',
 }
 
 export = config;
--- app/frontend/src/components/Balance.tsx
+++ app/frontend/src/components/Balance.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import MoneyBag from '../images/money-bag.png';
 
 import { getItem } from '../helpers/localStorage'
