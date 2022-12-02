8b4904bcbe837a58ad8373e24f383f04660f42fd
Seu nome <halisterfernando@hotmail.com>
2022-12-02 10:53:53 -0300

    chore: :rocket: dotenv e linter

--- app/frontend/src/components/PageFooter.tsx
+++ app/frontend/src/components/PageFooter.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { GiCoffeeCup } from "react-icons/gi";
 
 const PageFooter = () => {
- return <footer className="bg-black p-3 text-white">footer</footer>;
+ return (
+  <footer className="bg-black p-3 text-white">
+   <div className="text-sm text-center relative">
+    Desenvolvido por Halister Fernando dos Santos utilizando TypeScript, React,
+    TailwindCSS, NodeJS, Sequelize e várias canecas de café{" "}
+    <i className="text-lg text-ng-green absolute ml-2">
+     <GiCoffeeCup />
+    </i>
+   </div>  
+  </footer>
+ );
 };
 
 export default PageFooter;
